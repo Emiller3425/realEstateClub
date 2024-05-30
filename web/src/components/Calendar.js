1ad7cc9daa9b5b2f11d80189ba8b955fc37c34e4
Ethan Miller <milleret@mail.gvsu.edu>
2024-05-29 21:46:02 -0400

    fixed calendar url

--- web/src/components/Calendar.js
+++ web/src/components/Calendar.js
@@ -3,10 +3,9 @@ const Calendar = () => {
       <div>
         <h2>My Google Calendar</h2>
         <iframe
-          src="https://calendar.google.com/calendar/embed?src=your_calendar_id%40gmail.com&ctz=America/New_York"
-          style={{ border: 0, width: '800px', height: '600px' }}
+          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FDetroit&bgcolor=%23ffffff&src=ZW1pbGxlcjM0MjVAZ21haWwuY29t&src=ZTVmNDkxMTQ0YzBkYjg5NDAxODgwODM1Y2FjMGIxZjM4OGM0NjNlNTEzNDhhZjljYTYzMTJjYzRhYTA4ZGJiOEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%23EF6C00&color=%2333B679&color=%230B8043"
+          style={{ border: 4, width: '800px', height: '600px' }}
           frameBorder="0"
-          scrolling="no"
         ></iframe>
       </div>
     );

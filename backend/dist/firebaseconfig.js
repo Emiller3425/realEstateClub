1e510cd7acfaa8479b76ef0ec493a78e79bf29e5
Ethan Miller <milleret@mail.gvsu.edu>
2025-06-09 18:36:13 -0400

    please

--- backend/dist/firebaseconfig.js
+++ backend/dist/firebaseconfig.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
 // Initialize the Firebase app with a service account, granting admin privileges
-const serviceAccount = require('./config/realestateclub-584d7-firebase-adminsdk-5wrv6-6fe577f264.json');
+const serviceAccount = require('./config/realestateclub-584d7-firebase-adminsdk-5wrv6-0ab82c960c.json');
 firebase_admin_1.default.initializeApp({
     credential: firebase_admin_1.default.credential.cert(serviceAccount),
     storageBucket: 'gs://realestateclub-584d7.appspot.com'
